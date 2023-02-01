7cd96c2f12943668fe5441d656e7a50f51c35660
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-02-01 15:29:26 -0400

    Add crontab

--- Software/Paneldactyl/entrypoint.sh
+++ Software/Paneldactyl/entrypoint.sh
@@ -7,5 +7,8 @@ cd /home/container
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
+echo "executando crontab"
+nohup /usr/sbin/crond -f -l 8 &
+
 # Run the Server
 ${MODIFIED_STARTUP}
