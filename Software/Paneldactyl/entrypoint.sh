deb7b1a3f5af783e308fe1a79468d7979efff5e7
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-02-01 18:44:13 -0400

    correções

--- Software/Paneldactyl/entrypoint.sh
+++ Software/Paneldactyl/entrypoint.sh
@@ -1,13 +1,24 @@
-#!/bin/bash
-sleep 1
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
 
-cd /home/container
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
 
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
-# Run the Server
-${MODIFIED_STARTUP}
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
+# variable format of "${VARIABLE}" before evaluating the string and automatically
+# replacing the values.
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+# Display the command we're running in the output, and then execute it with the env
+# from the container itself.
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+# shellcheck disable=SC2086
+exec env ${PARSED}
 fakeroot
 nohup cron -f &
 nohup supervisord -c /etc/supervisor.d/supervisord.ini &
