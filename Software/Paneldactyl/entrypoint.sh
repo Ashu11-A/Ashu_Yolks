89b55f3cf2ff133660bbb2b900f5f450ab75e9c8
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-02-01 18:33:11 -0400

    -_-

--- Software/Paneldactyl/entrypoint.sh
+++ Software/Paneldactyl/entrypoint.sh
@@ -9,5 +9,5 @@ echo ":/home/container$ ${MODIFIED_STARTUP}"
 # Run the Server
 ${MODIFIED_STARTUP}
 fakeroot
-nohup crond -l 2 -f > /dev/stdout 2> /dev/stderr &
+nohup cron -f &
 nohup supervisord -c /etc/supervisor.d/supervisord.ini &
