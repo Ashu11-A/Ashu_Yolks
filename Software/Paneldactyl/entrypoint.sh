867610f93dcc08c08b9a043beb56d12691b64f4d
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-02-02 12:07:08 -0400

    Voltar para Alpine

--- Software/Paneldactyl/entrypoint.sh
+++ Software/Paneldactyl/entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
+sleep 1
+
 cd /home/container
 
-# Make internal Docker IP address available to processes.
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
-
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo -e ":/home/container$ ${MODIFIED_STARTUP}"
-
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 # Run the Server
-eval ${MODIFIED_STARTUP}
+${MODIFIED_STARTUP}
 fakeroot
-nohup cron -f &
+nohup crond -l 2 -f > /dev/stdout 2> /dev/stderr &
 nohup supervisord -c /etc/supervisor.d/supervisord.ini &
