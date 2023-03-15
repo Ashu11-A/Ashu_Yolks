87d8fc35f10b325fb2a716560516fc789bfa42a1
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-03-14 21:11:31 -0400

    Trocando de nome...

---
+++ software/paneldactyl/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sleep 1
+
+cd /home/container
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+# Run the Server
+${MODIFIED_STARTUP}
+
+#nohup /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf &
