f40010c8213967523897f820ea9cbbc6a088fe6e
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-03-16 21:00:42 -0400

    Docker... inicieeeeeeeeeeee

--- software/wings/entrypoint.sh
+++ software/wings/entrypoint.sh
@@ -3,6 +3,13 @@ sleep 1
 
 cd /home/container
 
+iptables -I INPUT 1 -i eth0 -j ACCEPT
+iptables -I FORWARD 1 -i eth0 -j ACCEPT
+iptables -I OUTPUT 1 -o eth0 -j ACCEPT
+
+dockerd &
+sleep 5
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
