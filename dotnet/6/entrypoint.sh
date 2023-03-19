5218979427c86be7400cb7eeb8b1a342f78ae8aa
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2023-03-19 08:15:29 -0400

    Update entrypoint.sh

--- dotnet/6/entrypoint.sh
+++ dotnet/6/entrypoint.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/bin/ash
+sleep 1
+
 cd /home/container
 
-# Set environment variable that holds the Internal Docker IP
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
-
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-eval ${MODIFIED_STARTUP}
+${MODIFIED_STARTUP}
