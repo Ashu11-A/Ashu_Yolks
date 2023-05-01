a96c642b5e5441db1a0845224673f0926b3ebb7d
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-30 23:21:37 -0400

    Alpine < Ubuntu

--- software/bot4all/entrypoint.sh
+++ software/bot4all/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-cd /home/container || exit
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
