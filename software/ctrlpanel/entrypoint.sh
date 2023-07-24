0b95dc82dd6e7161a586332c941525b5508e8599
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-07-24 17:18:54 -0400

    Alpline is not compatible

--- software/ctrlpanel/entrypoint.sh
+++ software/ctrlpanel/entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-sleep 1
-
 cd /home/container
 
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
-# Run the Server
-${MODIFIED_STARTUP}
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
 
-#nohup /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf &
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
+fakeroot
+nohup cron -f &
+nohup supervisord -c /etc/supervisor.d/supervisord.ini &
