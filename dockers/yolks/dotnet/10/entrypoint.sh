680e566983a846417cd6cc6490188c3d0aa15179
Matheus Nilton Biolowons <30575805+Ashu11-A@users.noreply.github.com>
2026-01-10 21:09:24 -0400

    🏔️ Adding new versions of dotnet and switching to arm64

---
+++ dockers/yolks/dotnet/10/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/ash
+sleep 1
+
+cd /home/container
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
