97711dc29f0ce4392a6c0d373c5feb67c5423e4d
Matheus Nilton Biolowons <30575805+Ashu11-A@users.noreply.github.com>
2026-02-08 14:29:44 -0400

    Fix entrypoint.sh

--- dockers/yolks/bun-nodejs/entrypoint.sh
+++ dockers/yolks/bun-nodejs/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-cd /home/container || exit 1
+cd /home/container
 
-MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
-${MODIFIED_STARTUP}
+# Run the Server
+eval ${MODIFIED_STARTUP}
