e7fead45005e8641647d8f97d0ef80768edea84f
Matheus Nilton Biolowons <30575805+Ashu11-A@users.noreply.github.com>
2026-07-09 19:39:48 -0300

    🥐 Adding Bun + Puppeteer Docker image

---
+++ dockers/yolks/bun-puppeteer/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/container
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
